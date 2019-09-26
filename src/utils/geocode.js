0c459aab2440c4a9ffdfad9dc6f5ed248c616028
unknown <fraoulmassresha@gmail.com>
2019-09-26 14:42:11 +0300

    reverted all changes

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -6,7 +6,7 @@ const request = require('request')
 const geocode =(address, callback)=>{
        const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=pk.eyJ1IjoiZnJhb3VsIiwiYSI6ImNqenh0N3Y4aDBsdGszam80Y3lmaHpwN3IifQ.lWqQNEmctUDs2BuAcLIx4g'
 
-    request({url, json:true},(error, {body}={})=>{
+    request({url, json:true},(error, {body})=>{
     
         // "const {body}=response" by distructuring it ,this is one way of putting it and the secound ways is that response is an object so we can {body}<= do this
     if (error){
