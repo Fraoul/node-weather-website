fe19540ff0994ad9fb0d365df147e08035876869
unknown <fraoulmassresha@gmail.com>
2019-09-26 11:08:30 +0300

    latest update

--- Public/Js/app.js
+++ Public/Js/app.js
@@ -1,34 +1,24 @@
-const weatherForm = document.querySelector('form')   
+const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
-const p1 =document.querySelector('#message-1')
-const p2 = document.querySelector('#message-2')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
 
-
-
-weatherForm.addEventListener('submit',(e) => {
+weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
-    
-    const location = search.value    
 
-    if (location.length === 0){
-        return p1.textContent = 'Enter search location '
-    }
-    
-    p1.textContent = 'Loading Message'
-    p2.textContent = ' '
+    const location = search.value
 
-    fetch('/weather?address='+location).then((response) => {
+    messageOne.textContent = 'Loading...'
+    messageTwo.textContent = ''
+
+    fetch('/weather?address=' + location).then((response) => {
         response.json().then((data) => {
-            if (data.error){
-                p1.textContent = (data.error)             // was console.log changed it to be printed on the browser
-                p2.textContent = ''
-            }else{
-                p1.textContent = (data.location)
-                p2.textContent = (data.forecast)
+            if (data.error) {
+                messageOne.textContent = data.error
+            } else {
+                messageOne.textContent = data.location
+                messageTwo.textContent = data.forecast
             }
         })
-    }) 
-           
-
-
+    })
 })
